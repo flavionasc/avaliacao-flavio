4838bf17204c28b227cb36cee84210302f73323b
flavionasc <flavio_radioativa@hotmail.com>
2015-11-02 11:24:12 -0200

    versao 1.0 completa

--- app/SalaryService.js
+++ app/SalaryService.js
@@ -3,8 +3,8 @@ var SalaryService = {
 	getSalary: function(salary) {
         var result = [0,0,0];   
         var totalINSS = 0.0;
-        var totalIrpf = 0.0;
-        var baseSalary = salary-totalINSS;
+        var totalIRPF = 0.0;
+        
        
             
         if (typeof(salary) === 'number'){
@@ -23,26 +23,28 @@ var SalaryService = {
 	                 totalINSS = 513.02;
 	            }
         
-        
+                var baseSalary = salary-totalINSS;
                 //deduçao irpf.
-                 totalIRPF = 0.0;
 	            
-	             if (baseSalary >= 1903.99 && baseSalary <= 2826.65){
+                 if(salary <= 1903.98) {
+				      totalIRPF = 0;
+			      } 
+	              else if (baseSalary > 1903.98 && baseSalary <= 2826.65){
 	                  totalIRPF = (baseSalary * 0.075) - 142.80 ;
 	             }
-                  else if(baseSalary >= 2826.66 && baseSalary <= 3751.05){
+                  else if(baseSalary > 2826.65 && baseSalary <= 3751.05){
                      totalIRPF =  (baseSalary * 0.15) - 354.80 ;
 	             }
-                  else if(baseSalary >= 3751.06 && baseSalary <= 4664.68){
+                  else if(baseSalary > 3751.05 && baseSalary <= 4664.68){
                       totalIRPF =  (baseSalary * 0.225) - 636.13 ;
 	             }
-	               else if(baseSalary > 4664.68){
-	                 totalIRPF = (baseSalary * 0.275) - 869.36;
+	               else{
+	                 totalIRPF = (baseSalary * 0.255) - 869.36;
 	            }
         
                result[0]=totalINSS.toFixed(2);
                result[1]=totalIRPF.toFixed(2);
-               result[2]=(salary - totalINSS)-totalIRPF.toFixed(2);
+               result[2]=baseSalary-totalIRPF.toFixed(2);
         
         
             return result;
